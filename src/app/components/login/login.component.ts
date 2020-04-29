687aa4a1d684c5ab71334760202ae0a376dc08b4
xgioserx <xgioserx@gmail.com>
2020-04-28 23:00:22 -0500

    ensayo login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
+import { UserInterface } from 'src/app/models/user-interface';
+import { isError } from 'util';
 // import {FlashMessagesService} from 'angularfire2-flash-messages';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
+
 })
 export class LoginComponent implements OnInit {
 
@@ -19,22 +22,35 @@ export class LoginComponent implements OnInit {
     // public flashMensaje: FlashMessagesService
   ) { }
 
+  private user: UserInterface;
+
   ngOnInit(): void {
   }
 
   onSubmitLogin() {
-    this.authService.loginEmail(this.email, this.password)
-    .then( (res) => {
-      this.router.navigate(['/home']);
-      // this.flashMensaje.show('Usuario logado correctamente.',
-      // {cssClass: 'alert-success', timeout: 4000});
-      // this.router.navigate(['/privado']);
-    }).catch((err) => {
-      // this.flashMensaje.show(err.message,
-      // {cssClass: 'alert-danger', timeout: 4000});
-      console.log(err)
-      this.router.navigate(['/login']);
-    });
+    return this.authService.loginuser(this.user.email, this.user.password)
+      .subscribe(
+        data => {
+          this.authService.setUser(data.user);
+          const token = data.id;
+          this.authService.setToken(token);
+          this.router.navigate(['/home']);
+        },
+        error => this.onIsError()
+      );
+
+    /*  this.authService.loginEmail(this.email, this.password)
+     .then( (res) => {
+       this.router.navigate(['/home']);
+       // this.flashMensaje.show('Usuario logado correctamente.',
+       // {cssClass: 'alert-success', timeout: 4000});
+       // this.router.navigate(['/privado']);
+     }).catch((err) => {
+       // this.flashMensaje.show(err.message,
+       // {cssClass: 'alert-danger', timeout: 4000});
+       console.log(err)
+       this.router.navigate(['/login']);
+     }); */
   }
 
 
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,7 +3,11 @@ import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
 import 'rxjs/add/operator/map';
 import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { UserInterface } from "../models/user-interface";
+import { UserInterface } from 'src/app/models/user-interface';
+import { isNullOrUndefined } from "util";
+import { Observable } from "rxjs/internal/Observable";
+import { map } from "rxjs/operators";
+//import { UserInterface } from "../models/user-interface";
 // import { map } from "rxjs/operators";
 // import 'rxjs/add/operator/catch';
 
@@ -21,31 +25,65 @@ export class AuthService {
     "Content-Type": "application/json"
   });
 
-  loginEmail(email: string, pass: string) {
+  loginuser(email: string, password: string): Observable<any> {
+    const url_api = "http://173.230.136.51:3000/api/login?include=user";
+    return this.htttp
+      .post<UserInterface>(
+        url_api,
+        { email, password },
+        { headers: this.headers }
+      )
+      .pipe(map(data => data));
+  }
+
+  setUser(user: UserInterface): void {
+    let user_string = JSON.stringify(user);
+    localStorage.setItem("currentUser", user_string);
+  }
+
+  setToken(token): void {
+    localStorage.setItem("accessToken", token);
+  }
+
+  getToken() {
+    return localStorage.getItem("accessToken");
+  }
+
+  getCurrentUser(): UserInterface {
+    let user_string = localStorage.getItem("currentUser");
+    if (!isNullOrUndefined(user_string)) {
+      let user: UserInterface = JSON.parse(user_string);
+      return user;
+    } else {
+      return null;
+    }
+  }
+
+  /* loginEmail(email: string, pass: string) {
     return new Promise((resolve, reject) => {
       this.afAuth.auth.signInWithEmailAndPassword(email, pass)
         .then(userData => resolve(userData),
           err => reject(err));
     });
   }
-
-  getAuth() {
+ */
+  /* getAuth() {
     // return this.afAuth.authState.pipe(map(auth => auth));
     return this.afAuth.authState.map(auth => auth);
     // map(auth => auth)
-  }
+  } */
+
+  /*  logout() {
+     return this.afAuth.auth.signOut();
+   } */
 
   logout() {
-    return this.afAuth.auth.signOut();
+    let accessToken = localStorage.getItem("accessToken");
+    const url_api = `http://173.230.136.51:3000/api/logout?access_token=${accessToken}`;
+    console.log(url_api);
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("currentUser");
+    return this.htttp.post<UserInterface>(url_api, { headers: this.headers });
   }
-
-  // logout() {
-  //   let accessToken = localStorage.getItem("accessToken");
-  //   const url_api = `http://173.230.136.51:3000/api/logout?access_token=${accessToken}`;
-  //   console.log(url_api);
-  //   localStorage.removeItem("accessToken");
-  //   localStorage.removeItem("currentUser");
-  //   return this.htttp.post<UserInterface>(url_api, { headers: this.headers });
-  // }
 
 }
